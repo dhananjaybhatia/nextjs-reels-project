1cc66c70375e58bacf9d493557b64c11ddc6b927
dhananjaybhatia <dhananjaybhatia@yahoo.co.in>
2025-05-04 13:36:04 +1000

    UserModel is created

---
+++ models/User.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface IUser {
+    email: string;
+    password: string;
+    _id?: mongoose.Types.ObjectId;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const userSchema = new mongoose.Schema<IUser>(
+    {
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: [true, "Email already exist"],
+            trim: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            minlength: [5, "Password must be at least 5 characters"],
+        },
+    },
+    { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash("this.password", 10);
+    }
+    next();
+});
+
+// ✅ Reuse existing model if already defined (helps avoid OverwriteModelError during hot-reloading in Next.js)
+
+const User = mongoose.models?.User || mongoose.model<IUser>("User", userSchema)
+export default User
